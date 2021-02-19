e0ef7e6bb554711b53c54e0bda121a8c64209f99
RendiJuli <juliarto.rendi@gmail.com>
2021-02-19 15:02:47 +0700

    Splash,SwipeRefresh,API,time

--- app/src/main/java/com/example/koranku/Adapter.java
+++ app/src/main/java/com/example/koranku/Adapter.java
@@ -14,8 +14,16 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.koranku.Model.Articles;
 import com.squareup.picasso.Picasso;
 
+import org.ocpsoft.prettytime.PrettyTime;
+
 import java.lang.reflect.Array;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
+
+import static java.util.Locale.ENGLISH;
 
 public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
@@ -31,7 +39,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
     @Override
     public Adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.items, parent, false);
-        return new  ViewHolder (view);
+        return new ViewHolder(view);
     }
 
     @Override
@@ -45,7 +53,6 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
         String imageUrl = a.getUrlToImage();
 
         Picasso.with(context).load(imageUrl).into(holder.imageView);
-
 
 
     }
@@ -73,4 +80,28 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
         }
     }
+
+    public String dateTime(String t) {
+
+        PrettyTime prettyTime = new PrettyTime(new Locale(getCountry()));
+        String time = null;
+
+        try {
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:", Locale.ENGLISH);
+            Date date = simpleDateFormat.parse(t);
+            time = prettyTime.format(date);
+        } catch (ParseException e){
+
+            e.printStackTrace();
+        }
+        return time;
+    }
+
+
+    public String getCountry() {
+        Locale locale = Locale.getDefault();
+        String country = locale.getCountry();
+        return country.toLowerCase();
+    }
+
 }
--- app/src/main/java/com/example/koranku/MainActivity.java
+++ app/src/main/java/com/example/koranku/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.koranku;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
 import android.widget.Toast;
@@ -20,7 +21,7 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    SwipeRefreshLayout swipeRefreshLayout;
     RecyclerView recyclerView;
     final String API_KEY = "51c2fae0aa354e8a91c8ef981a395e92";
     Adapter adapter;
@@ -32,20 +33,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        swipeRefreshLayout = findViewById(R.id.swipeRefresh);
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         String country = getCountry();
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                loadJson(country, API_KEY);
+            }
+        });
+
+
         loadJson(country, API_KEY);
     }
 
     public void loadJson(String country, String apiKey){
-
+        swipeRefreshLayout.setRefreshing(true);
         Call<News> call = ApiClient.getInstance().getApi().getNews(country, apiKey);
         call.enqueue(new Callback<News>() {
             @Override
             public void onResponse(Call<News> call, Response<News> response) {
                 if (response.isSuccessful() && response.body().getArticles() != null){
-
+                        swipeRefreshLayout.setRefreshing(false);
                         articles.clear();
                         articles = response.body().getArticles();
                         adapter = new Adapter(MainActivity.this,articles);
@@ -56,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<News> call, Throwable t) {
+                swipeRefreshLayout.setRefreshing(false);
                 Toast.makeText(MainActivity.this, t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
 
 
